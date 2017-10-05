d4e62b19874a5759be05a165d152e5f316d152af
Swarts, Matthew E <strontium5@gmail.com>
2017-10-05 04:02:57 -0400

    default port 80, but allow heroku to control

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
-process.env.PORT = 80;
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 
 
 
